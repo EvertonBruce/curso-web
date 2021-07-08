f2c6f2cfd11254528662feebf4bfb6e2db45b572
Everton Bruce <evertonflbruce@gmail.com>
2021-07-07 23:18:37 -0300

    Flappy Bird resolução.

--- exercicios-web/dom/js/flappy.js
+++ exercicios-web/dom/js/flappy.js
@@ -1,12 +1,1 @@
 const bird = document.querySelector('.bird')
-
-bird.style.position = 'absolute'
-
-document.addEventListener('keydown', (e) => {
-    bird.style.bottom = `${bird.clientHeight + 20}px`;
-    console.log(bird.style.bottom)
-})
-
-setTimeout(()=> {
-    bird
-}, 500)
