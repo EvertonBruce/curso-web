2eb0a3ac54ca974924af19b89c180530603b34a8
Everton <everton@tecnasistemas.com.br>
2021-07-16 17:16:27 -0300

    Usando o webpack para compilar arquivos js e css.

--- exercicios-web/webpack/webpack.config.js
+++ exercicios-web/webpack/webpack.config.js
@@ -1,7 +1,27 @@
-const { Module } = require('webpack')
+// const { Module } = require('webpack')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/principal.js'
+  entry: './src/principal.js',
+  output: {
+    filename: 'principal.js',
+    path: __dirname + '/public'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css"
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        //'style-loader', // Adicionar CSS a DOM injetando a tag <style>
+        'css-loader' // interpreta @import, url()..
+      ]
+    }]
+  }
 }
